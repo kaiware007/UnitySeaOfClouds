4f58043c8e5d33af92ff9d645ae8f3122ffa1b09
matsudayuma <matsudayuma@team-lab.com>
2017-12-04 04:07:45 +0800

    tweak

---
+++ Assets/Sources/DepthWriter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthWriter : CustomImageEffect {
+
+    public RenderTexture depthTexture;
+
+    private Camera cam;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+
+        depthTexture = new RenderTexture(Screen.width, Screen.height, 32, RenderTextureFormat.Depth);
+        depthTexture.wrapMode = TextureWrapMode.Clamp;
+        depthTexture.filterMode = FilterMode.Bilinear;
+        depthTexture.antiAliasing = 1;
+        depthTexture.dimension = UnityEngine.Rendering.TextureDimension.Tex2D;
+        depthTexture.depth = 24;
+        depthTexture.useMipMap = false;
+        depthTexture.autoGenerateMips = false;
+        depthTexture.anisoLevel = 0;
+        depthTexture.hideFlags = HideFlags.HideAndDontSave;
+        depthTexture.Create();
+
+        if (cam == null)
+        {
+            cam = GetComponent<Camera>();
+            cam.depthTextureMode = DepthTextureMode.Depth;
+        }
+
+       cam.targetTexture = depthTexture;
+       
+    }
+
+    public override string ShaderName
+    {
+        get { return "Custom/CustomDepth"; }
+    }
+
+    protected override void UpdateMaterial()
+    {
+        Shader.SetGlobalTexture("_CustomCameraDepthTexture", depthTexture );
+    }
+
+}
